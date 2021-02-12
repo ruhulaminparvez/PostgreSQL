1a9688d90a7014d3d6ca6e2891d76c2c387df963
Ruhul12 <ruhulaminparvez007@gmail.com>
2021-02-12 22:03:35 +0600

    Movie Rental System Project Create

---
+++ Movie-Rental_System/input-movie.py
@@ -0,0 +1,7 @@
+from movie import Movie
+
+my_movies = Movie("Stranger Things", "Sci-fi")
+
+print(my_movies.name)
+print(my_movies.genre)
+
---
+++ Movie-Rental_System/movie.py
@@ -0,0 +1,4 @@
+class Movie:
+    def __init__(self, name, genre):
+        self.name = name
+        self.genre = genre
